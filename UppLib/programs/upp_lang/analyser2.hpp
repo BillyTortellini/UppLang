3d4edbf72b9658334a10898cf158016c0ca29731
MartinR <martin.rupp@gmx.net>
2022-05-03 09:31:47 +0200

    Reset Point before Removing RC-Program

--- UppLib/programs/upp_lang/analyser2.hpp
+++ UppLib/programs/upp_lang/analyser2.hpp
@@ -1,6 +1,16 @@
 #pragma once
 
 /*
+Approach:
+ - Rewrite RC-Analyser so that it works with the new AST
+ - Replace RC-Tree with AST in Semantic Analysis
+ - Should be compilable
+
+ - Remove Modtree and annotate AST instead
+
+
+
+
 Goals with new Analyser:
  - Use AST with Semantic Information
  - Remove unnecessary Stages/Data-Representation from Process (RC-Code, Modtree)
@@ -9,11 +19,17 @@ Goals with new Analyser:
 
 Problems with current Approach:
  - using Statements
+    This is just a problem with Symbol-Resolution, not a problem of workloads
  - Polymorphic Functions/Templates (Analysis + Instanciation)
+    Just requires annotating the AST multiple times, altough determining
+    the order of Argument Evaluation is still tricky
  - includes
  - Function_Cluster_Compile
  - Struct_Reachable_Resolve
  - Bake Expressions
+ - Macro Symbols
+    Symbol Resolution
+ - Conditional Compilation
 
 My previous approaches were:
  * Stop and Go with Statement Granularity:
@@ -36,6 +52,20 @@ Further Problems:
     using Datastructures~Hashtable
 
 Potential Solution: Stop and Go with Fiber Granularity + Previous Discovery Stage
+    When no workloads can be run, we go and detect circular dependencies. Esy
+    Struct Reachable is then also not needed anymore, since this can be handled with stop and go.
+    Function Cluster Compile? 
+        A function can only be executed when all function it calls can be executed
+        I think I have the same problem as with the stop and go.
+    Struct Reachable Resolve?
+    Struct Size is also Cluster -> No, since Size must not have Circular Dependency
+    Bake?
+
+Why and how to annotate AST?
+    Otherwise I need to store the same information again (Operation Type, Expression Type, General Block-Structure)
+    but this time only with Type/Symbol/Cast-Information. With the AST, I don't have to worry about 
+    getting the structure wrong. Also, since things with Semantic Errors don't get compiled, I don't
+    have to worry about generating Code for a non-compiled Structure
 
 Problem: Symbol Discovery/Symbol Tables
 */
